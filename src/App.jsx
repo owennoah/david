48b4a2a77d4b688ff342f68e8dc3f9e3350ddb8f
Umar <joyiaumar266@gmail.com>
2026-07-03 00:19:00 +0500

    fix: corrected missing closing divs in projects section

--- src/App.jsx
+++ src/App.jsx
@@ -486,6 +486,7 @@ function App() {
                   </button>
                 </div>
               </div>
+              </div>
               )}
 
               {/* Project 2 */}
@@ -536,6 +537,7 @@ function App() {
                   </button>
                 </div>
               </div>
+              </div>
               )}
 
               {/* Project 3 */}
@@ -585,6 +587,7 @@ function App() {
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
                   </button>
                 </div>
+              </div>
               </div>
               )}
 
